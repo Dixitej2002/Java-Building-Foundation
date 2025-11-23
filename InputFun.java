1633d29af52bf23cbb61a07c8428d13ae6ce24f0
I AM Dixit <sik.amitkumar@gmail.com>
2025-11-23 18:38:16 +0530

    Patern updated PQ

---
+++ InputFun.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputFun {
+
+    static int input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        return n;
+    }
+    
+}
---
+++ Pattern5.java
@@ -0,0 +1,18 @@
+public class Pattern5 {
+
+    public static void main(String[] args) {
+        int n = InputFun.input();
+        char ch = 'A';
+
+        for(int i=1 ; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                System.out.print(ch);
+                
+            }
+
+            System.out.println();
+            ch++;
+        }
+    }
+    
+}
